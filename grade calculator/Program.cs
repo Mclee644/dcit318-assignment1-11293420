bd42364917606506fe766f7f7310dc23730bce79
Mclee644 <644mcl@gmail.com>
2025-07-30 16:18:49 +0000

    adding input logic to the program

--- grade calculator/Program.cs
+++ grade calculator/Program.cs
@@ -1,7 +1,33 @@
 ﻿using System;
 
 class Program{
-	static void Main(string[] args){
-		Console.WriteLine("hola");
+	static void Main(string[] args)
+	{
+		string prompt = """
+
+		******************
+		GRADE CALCULATOR
+		******************
+		
+		""";
+		string terminalPrompt;
+		int score;
+		//KILL SWITCH
+		void Terminal(){
+			Console.WriteLine("Do you want exit the program: Y/N");
+			terminalPrompt = Console.ReadLine().ToUpper();		
+		}
+		//INPUT
+		void Input(){
+			Console.WriteLine("Enter your exam score: ");
+			score = Convert.ToInt32(Console.ReadLine());
+		}
+		//MAIN PROGRAM
+		do{
+		Console.WriteLine(prompt);	
+		Input();
+		Console.WriteLine(score);
+		Terminal();
+		}while(terminalPrompt != "Y");
 	}
 }
--- grade calculator/obj/Debug/net9.0/grade calculator.AssemblyInfo.cs
+++ grade calculator/obj/Debug/net9.0/grade calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("grade calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20038c616618a7fb4746fc3256c9faf77a908c03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c36670ca3e711c4ef95aedae7f7a2c7ac8dd02d")]
 [assembly: System.Reflection.AssemblyProductAttribute("grade calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("grade calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
