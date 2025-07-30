cae39ec6a7c070adb9c9b9d00a4eb662888e99e1
Mclee644 <644mcl@gmail.com>
2025-07-30 20:14:59 +0000

    ticking logic implement

--- ticket price calculator/Program.cs
+++ ticket price calculator/Program.cs
@@ -3,19 +3,51 @@
 class Program{
 	static void Main(string[] args){
 		bool terminalCondition = true;
-		string title = """
+		string title = $"""
 
-		******************************
-		*	TICKET PRICE CALCULATOR	 *
-		******************************	
+		*************************************
+			TICKET PRICE CALCULATOR	        
+		*************************************	
 
 		""";
+		string result;
 		string prompt;
+		int standardPrice = 10;
+		int ticketPrice;
+		int discount = 7;
+		int age;
+		int netPrice = standardPrice - discount; 
 
 		do{
 			Console.WriteLine(title);
+			Input();
+			switch(age){
+				case >= 65:
+					ticketPrice = netPrice;
+					break;
+				case <= 12:
+					ticketPrice = netPrice;
+					break;
+				default:
+					ticketPrice = standardPrice;
+					break;
+			}
+			result = $"""
+			+--------------------------+
+			+ AGE	| TICKET PRICE
+			+--------------------------+
+			+ {age} | GHc {ticketPrice}
+			+--------------------------+	
+
+			""";
+			Console.WriteLine(result);
 			Terminal();
 		}while(terminalCondition);
+
+		void Input(){
+			Console.WriteLine("Enter Your Age In Complete Years: ");
+			age = Convert.ToInt32(Console.ReadLine());
+		}
 		
 		void Terminal(){
 			Console.WriteLine("Do You Want To Quit: Y/N");
